4942034f6fe31f583c15de63d4da166275d478e7
Christopher Doris <github.com/cjdoris>
2022-01-29 18:16:25 +0000

    add PkgREPL tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using CondaPkg
 using Test
 
+# Only run the gc tests on CI (because it's annoying to do it locally)
+testgc = get(ENV, "CI", "") == "true"
+
 status() = sprint(io -> CondaPkg.status(io=io))
 
 @testset "CondaPkg" begin
@@ -15,7 +18,7 @@ status() = sprint(io -> CondaPkg.status(io=io))
 
     @testset "install python" begin
         @test !occursin("python", status())
-        CondaPkg.add("python")
+        CondaPkg.add("python", version="3.10.2")
         CondaPkg.resolve()
         CondaPkg.withenv() do
             pythonpath = joinpath(CondaPkg.envdir(), Sys.iswindows() ? "python.exe" : "bin/python")
@@ -75,10 +78,57 @@ status() = sprint(io -> CondaPkg.status(io=io))
         @test !isfile(curl_path)
     end
 
-    
-    @testset "gc()" begin
-        # verify that micromamba clean runs without errors
-        CondaPkg.gc()
+    if testgc
+        @testset "gc()" begin
+            # verify that micromamba clean runs without errors
+            CondaPkg.gc()
+        end
+    end
+
+    @testset "PkgREPL" begin
+        # The functions in CondaPkg.PkgREPL are not API
+        # but calling them is the closest thing to calling
+        # the Pkg REPL.
+
+        # add
+        CondaPkg.PkgREPL.add([" foo >=1.2 "])
+        @test occursin("foo", status())
+        @test occursin("(>=1.2)", status())
+
+        # rm
+        CondaPkg.PkgREPL.rm([" foo "])
+        @test !occursin("foo", status())
+
+        # add channel
+        CondaPkg.PkgREPL.add([" foo-channel "], mode=:channel)
+        @test occursin("foo-channel", status())
+
+        # rm channel
+        CondaPkg.PkgREPL.rm([" foo-channel "], mode=:channel)
+        @test !occursin("foo-channel", status())
+
+        # add pip
+        CondaPkg.PkgREPL.add([" foo ~=1.3 "], mode=:pip)
+        @test occursin("foo", status())
+        @test occursin("(~=1.3)", status())
+
+        # rm pip
+        CondaPkg.PkgREPL.rm([" foo "], mode=:pip)
+        @test !occursin("foo", status())
+
+        # status
+        # TODO: capture the output and check it equals status()
+        CondaPkg.PkgREPL.status()
+
+        # resolve
+        CondaPkg.PkgREPL.resolve()
+
+        # gc
+        testgc && CondaPkg.PkgREPL.gc()
+
+        # run
+        # TODO: capture the output and check it contains "Python 3.10.2"
+        CondaPkg.PkgREPL.run(["python", "--version"])
     end
 
 end
