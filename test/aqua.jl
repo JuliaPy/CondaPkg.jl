366a7eaa9aececf8f4597c4621fc56905e11da37
Christopher Doris <github.com/cjdoris>
2024-08-15 15:53:02 +0100

    add aqua tests

---
+++ test/aqua.jl
@@ -0,0 +1,4 @@
+@testitem "Aqua" begin
+    import Aqua
+    Aqua.test_all(CondaPkg)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -14,4 +14,10 @@ if get(ENV, "CI", "false") == "false"
     delete!(ENV, "JULIA_CONDAPKG_ENV")
 end
 
+# if you run tests in a conda environment, these env vars cause the aqua persistent tasks test to error
+if haskey(ENV, "CONDA_PREFIX")
+    delete!(ENV, "SSL_CERT_FILE")
+    delete!(ENV, "SSL_CERT_DIR")
+end
+
 @run_package_tests
