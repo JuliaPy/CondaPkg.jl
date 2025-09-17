990d074a9e807fc66efd22abf294e6aef5ca6911
Christopher Doris <github.com/cjdoris>
2025-09-17 21:32:14 +0100

    less strict merging of editable flag

--- src/resolve.jl
+++ src/resolve.jl
@@ -518,7 +518,7 @@ function _resolve_merge_pip_packages(packages)
         urls = String[]
         binary = ""
         extras = String[]
-        editables = Bool[]
+        editable = false
         for (fn, pkg) in pkgs
             @assert pkg.name == name
             if startswith(pkg.version, "@")
@@ -540,7 +540,7 @@ function _resolve_merge_pip_packages(packages)
                 end
             end
             append!(extras, pkg.extras)
-            push!(editables, pkg.editable)
+            editable |= pkg.editable
         end
         sort!(unique!(urls))
         sort!(unique!(versions))
@@ -557,12 +557,16 @@ function _resolve_merge_pip_packages(packages)
                 "direct references ('@ ...') and version specifiers both given for pip package '$name'",
             )
         end
-        unique!(editables)
-        if length(editables) != 1
-            error("both 'editable = true' and 'editable = false' specified for pip package '$name'")
-        end
-        editable = only(editables)
-        push!(specs, PipPkgSpec(name, version = version, binary = binary, extras = extras, editable = editable))
+        push!(
+            specs,
+            PipPkgSpec(
+                name,
+                version = version,
+                binary = binary,
+                extras = extras,
+                editable = editable,
+            ),
+        )
     end
     sort!(specs, by = x -> x.name)
 end
@@ -679,7 +683,7 @@ function _resolve_pip_install(io, pip_specs, load_path, backend)
         end
         if spec.editable
             # remove the @ from the beginning of the path.
-            url = replace(spec.version, r"@\s*"=>"")
+            url = replace(spec.version, r"@\s*" => "")
             push!(args, "--editable", url)
         end
     end
